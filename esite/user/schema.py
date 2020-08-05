66e2a168acdb18d152f6b7a7a4f7edf762c5066e
schettn <nicoschett@icloud.com>
2020-08-05 19:38:09 +0200

    adjust user schema

--- esite/user/schema.py
+++ esite/user/schema.py
@@ -21,33 +21,11 @@ class UserType(DjangoObjectType):
         exclude_fields = ["password"]
 
 
-class CreateUser(graphene.Mutation):
-    user = graphene.Field(UserType)
-
-    class Arguments:
-        username = graphene.String(required=True)
-        password = graphene.String(required=True)
-        email = graphene.String(required=True)
-
-    @superuser_required
-    def mutate(self, info, username, password, email):
-        user = get_user_model()(username=username, email=email,)
-
-        user.set_password(password)
-        user.save()
-
-        return CreateUser(user=user)
-
-
-class Mutation(graphene.ObjectType):
-    create_user = CreateUser.Field()
-
-
 class Query(graphene.ObjectType):
     me = graphene.Field(UserType, token=graphene.String(required=False))
     users = graphene.List(UserType, token=graphene.String(required=False))
 
-    @superuser_required
+    @login_required
     def resolve_users(self, info, **_kwargs):
 
         return User.objects.all()
