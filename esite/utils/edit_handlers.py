a3d2ad9f2153ff882c5e299de01f424f7815f7e8
schettn <nicoschett@icloud.com>
2020-08-16 17:22:44 +0200

    Remove TestPanel from utils edit handlers
    A unnecessary panel has been removed because
    it had no usage at all. Not even for testing.

--- esite/utils/edit_handlers.py
+++ esite/utils/edit_handlers.py
@@ -45,11 +45,3 @@ class ReadOnlyPanel(EditHandler):
             _(":"),
             self.render(),
         )
-
-
-class TestPanel(EditHandler):
-    def __init__(self, attr, *args, **kwargs):
-        self.attr = attr
-        super().__init__(*args, **kwargs)
-
-        print(self.__dict__)
